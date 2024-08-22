ef87daa9edbd83d263d831de17f2bb665ec0e8a5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-23 04:09:39 +0700

    test : list & set

--- test/redis.test.js
+++ test/redis.test.js
@@ -6,12 +6,12 @@ const _initialRedis= () => {
             host:process.env.HOST,
             port:process.env.PORT,
             db:process.env.DB,
-            username:process.env.username,
-            password:process.env.password
+            username:process.env.USERNAME_REDIS,
+            password:process.env.PASSWORD
     });
 }
  
-describe("connect to redis server", () => {
+describe.skip("connect to redis server", () => {
     let redis=null;
 
     beforeEach(async() => {
@@ -28,7 +28,7 @@ describe("connect to redis server", () => {
     });
 })
 
-describe("string", () => {
+describe.skip("string", () => {
     let redis=null;
 
     beforeEach(async() => {
@@ -49,3 +49,58 @@ describe("string", () => {
         expect(name).toBeNull();
     })
 })
+
+describe.skip("list", () =>{
+    let redis=null;
+
+    beforeEach(async() => {
+        redis=_initialRedis()
+    });
+
+    afterEach(async() => {
+        await redis.del("names"); 
+        await redis.quit();
+    });
+
+   it("should suppport list", async () => {
+        await redis.rpush("names","rama");
+        await redis.rpush("names","fajar");
+
+       expect(await redis.llen("names")).toBe(2);
+
+       const names=await redis.lrange("names",0,-1);
+       expect(names).toEqual(["rama","fajar"]);
+
+       expect(await redis.lpop("names")).toBe("rama");
+       expect(await redis.lpop("names")).toBe("fajar");
+
+       expect(await redis.llen("names")).toBe(0);
+   }); 
+});
+
+describe("set", () =>{
+    let redis=null;
+
+    beforeEach(async() => {
+        redis=_initialRedis()
+    });
+
+    afterEach(async() => {
+        await redis.del("names"); 
+        await redis.quit();
+    });
+
+   it("should support set", async() => {
+        await redis.sadd("names","rama");
+        await redis.sadd("names","rama"); 
+        await redis.sadd("names","fajar"); 
+        await redis.sadd("names","fajar"); 
+
+        expect(await redis.scard("names")).toBe(2);
+
+        const names=await redis.smembers("names");
+        expect(names).toEqual(["rama","fajar"]);
+   });
+});
+
+  
